3da9ceee3a4dc8bbdb84c5d91e2917a351afd0b3
pravinVIP <32026088+pravinVIP@users.noreply.github.com>
2025-07-16 08:04:04 +0530

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        List<String> list= Arrays.asList("a","b","c");
+        List<String> list2=List.of("a","b","c");
+        System.out.println(list);
+        System.out.println(list2);
+
+        String[] strArr={"a","b","c"};
+        Stream<String> list3=Arrays.stream(strArr).filter(s->!s.startsWith("a"));
+        List<String> list4=Arrays.stream(strArr).collect(Collectors.);
+
+
+
+        list3.forEach(System.out::println);
+
+        Arrays.stream(strArr,0,1).forEach(System.out::println);
+
+    }
+}
